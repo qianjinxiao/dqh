9a68a3ca8f5a795102c569a67f6943964f547574
小钱 <1132325868@qq.com>
2022-09-02 16:02:20 +0800

    no message

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -26,6 +26,7 @@ use App\Helpers\ResponseEnum;
 use App\Http\Controllers\Api\BaseController;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Http\Requests\UserLoginRequest;
+use App\Models\Farmland\Farmland;
 use App\Models\Inspect\InspectClockData;
 use App\Models\SmallReservoirs\SmallReservoir;
 use App\Models\User;
@@ -37,32 +38,79 @@ use Illuminate\Support\Facades\DB;
 
 class UserController extends BaseController
 {
-    public function home(Request $request){
-        $date=$request->input('date','');
-        if($date==''){
-            $m=Carbon::now();
-            $m1=Carbon::now();
-        }else{
-            $m=Carbon::parse($date);
-            $m1=Carbon::parse($date);
+    public function home(Request $request)
+    {
+        $date = $request->input('date', '');
+        if ($date == '') {
+            $m = Carbon::now();
+            $m1 = Carbon::now();
+        } else {
+            $m = Carbon::parse($date);
+            $m1 = Carbon::parse($date);
         }
-        $start=$m->startOfMonth();
-        $end=$m1->addMonth()->startOfMonth();
-       $data= InspectClockData::query()->select(DB::raw("CONCAT(YEAR(created_at),'-',MONTH(created_at)) as month,count(id) as day,project_type"))->where(
-           "created_at",">=",$start
-       )->where(
-           "created_at","<",$end
-       )->groupBy("project_type")->groupBy("month")->get()->each(function ($item)use ($m){
-           $item->name=ProjectEnum::$allTypeMap[$item->project_type];
-           $item->month_day=$m->daysInMonth;
-           return $item;
-       });
-        $data=$data->toArray();
-        foreach ($data as $k=>$v){
-//            $data[$k]['']
+        $start = $m->startOfMonth();
+        $end = $m1->addMonth()->startOfMonth();
+        $data = InspectClockData::query()->select(DB::raw("CONCAT(YEAR(created_at),'-',MONTH(created_at)) as month,count(id) as day,project_type"))->where(
+            "created_at", ">=", $start
+        )->where(
+            "created_at", "<", $end
+        )->where('project_type', '!=', Farmland::class)->groupBy("project_type")->groupBy("month")->get()->each(function ($item) use ($m) {
+            $item->name = ProjectEnum::$allTypeMap[$item->project_type];
+            $item->days = $m->daysInMonth;
+            $item->percent = round($item->day / $item->days * 100, 2) . '%';
+            return $item;
+        });
+
+        $farmland = InspectClockData::query()->select(DB::raw("count(id) as day,project_type"))->whereBetween(
+            "created_at", [Carbon::now()->startOfQuarter()->format("Y-m-d"), Carbon::now()->endOfQuarter()->format("Y-m-d")]
+        )->where('project_type', '=', Farmland::class)->groupBy("project_type")->first();
+        if ($farmland) {
+            $farmland->month = Carbon::now()->startOfQuarter()->format("Y-m") . '/' . Carbon::now()->endOfQuarter()->format("Y-m");
+            $farmland->name = ProjectEnum::$allTypeMap[$farmland->project_type];
+            $farmland->days = Carbon::now()->startOfQuarter()->diffInDays(Carbon::now()->endOfQuarter());
+            $farmland->percent = round($farmland->day / $farmland->days * 100, 2) . '%';
+            $farmland=$farmland->toArray();
+        } else {
+            $farmland=["month" => Carbon::now()->startOfQuarter()->format("Y-m") . '/' . Carbon::now()->endOfQuarter()->format("Y-m"),
+                "day" => 0,
+                "project_type" =>Farmland::class,
+                "name" => ProjectEnum::$allTypeMap[Farmland::class],
+                "days" => Carbon::now()->startOfQuarter()->diffInDays(Carbon::now()->endOfQuarter()),
+                "percent" => '0%',
+            ];
         }
+
+        $data = $data->toArray();
+        $arr = [];
+        foreach ($data as $k => $v) {
+            $arr[$v['project_type']] = $v;
+        }
+        $newArr = [];
+        $f_all_type = array_flip(ProjectEnum::$allTypeMap2);
+        foreach (ProjectEnum::$allType as $v) {
+            if (isset($arr[$f_all_type[$v]])) {
+                $newArr[$v] = $arr[$f_all_type[$v]];
+            } else {
+                $newArr[$v] = ["month" => Carbon::now()->format("Y-m"),
+                    "day" => 0,
+                    "project_type" => $f_all_type[$v],
+                    "name" => ProjectEnum::$allTypeMap[$f_all_type[$v]],
+                    "days" => $m->daysInMonth,
+                    "percent" => '0%',
+                ];
+            }
+        }
+        $newArr['small_reservoir_and_pool'] = $newArr['small_reservoir'];
+        $newArr['small_reservoir_and_pool']['name']='水库山塘';
+        $newArr['small_reservoir_and_pool']['day'] += $newArr['pool']['day'];
+        $newArr['small_reservoir_and_pool']['percent'] = round($newArr['small_reservoir_and_pool']['day'] / $newArr['small_reservoir_and_pool']['days'] * 100, 2) . '%';
+        $newArr['farmland'] = $farmland;
+        unset($newArr['small_reservoir']);
+        unset($newArr['pool']);
+        dd($newArr);
         return $this->success($m);
     }
+
     public function login(UserLoginRequest $request)
     {
         $username = $request->username;
@@ -80,16 +128,19 @@ class UserController extends BaseController
             'expires_in' => auth('api')->factory()->getTTL() * 60
         ]);
     }
-    public function mine(Request $request){
-        $user=$request->user();
-        $u=User::query()->with('project')->find($user->id);
-        if(isset($u->project)){
-            $u->project->project_type_name=ProjectEnum::$allTypeMap[$u->project->project_type];
-            $u->project->project_type=ProjectEnum::$allTypeMap2[$u->project->project_type];
-            $u->project->project_name=ProjectFactory::CreateProject($u->project->project_type)->where('id', $u->project->project_id)->value('name');
+
+    public function mine(Request $request)
+    {
+        $user = $request->user();
+        $u = User::query()->with('project')->find($user->id);
+        if (isset($u->project)) {
+            $u->project->project_type_name = ProjectEnum::$allTypeMap[$u->project->project_type];
+            $u->project->project_type = ProjectEnum::$allTypeMap2[$u->project->project_type];
+            $u->project->project_name = ProjectFactory::CreateProject($u->project->project_type)->where('id', $u->project->project_id)->value('name');
         }
         return $this->success($u);
     }
+
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
         $driver = \Socialite::create($type);
@@ -132,6 +183,7 @@ class UserController extends BaseController
         $token = auth('api')->login($user);
         return $this->respondWithToken($token);
     }
+
     public function bind($type, SocialAuthorizationRequest $request)
     {
         $driver = \Socialite::create($type);
@@ -157,13 +209,13 @@ class UserController extends BaseController
         switch ($type) {
             case 'wechat':
                 $unionid = $oauthUser->getRaw()['unionid'] ?? null;
-                $user=$request->user();
+                $user = $request->user();
                 if ($unionid) {
-                    $user->weixin_unionid=$unionid;
-                    $user->weixin_openid=$oauthUser->getId();
+                    $user->weixin_unionid = $unionid;
+                    $user->weixin_openid = $oauthUser->getId();
 
                 } else {
-                    $user->weixin_openid=$oauthUser->getId();
+                    $user->weixin_openid = $oauthUser->getId();
                 }
                 $user->save();
                 break;
